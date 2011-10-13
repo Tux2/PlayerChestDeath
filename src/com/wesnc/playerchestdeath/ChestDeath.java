1d3dfcea72a48d52c41752f820e36b751a4adf82
Joshua Reetz <josh100288@jrtechsupport.com>
2011-10-13 13:11:23 -0600

    Added Permissions 2.x/3.x and Bukkit SuperPerms support. Also changed name so that people don't get confused as far as the "official name".

--- src/com/wesnc/playerchestdeath/ChestDeath.java
+++ src/com/wesnc/playerchestdeath/ChestDeath.java
@@ -11,20 +11,23 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.griefcraft.lwc.*;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class ChestDeath extends JavaPlugin
 {
 	
 	Logger logger = Logger.getLogger("Minecraft");
 	
-	public String mainDir = "plugins/PCD/";
-	public File configFile = new File(mainDir+"PCDConfig.cfg");
+	public String mainDir = "plugins/DeadMansChest/";
+	public File configFile = new File(mainDir+"Config.cfg");
 	public Properties prop = new Properties();
 	public LinkedList<Block> nodropblocks = new LinkedList<Block>();
 	public ConcurrentHashMap<Block, Block> signblocks = new ConcurrentHashMap<Block, Block>();
@@ -38,35 +41,38 @@ public class ChestDeath extends JavaPlugin
 		"Death Message must be true for the death message String to work!" +
 		"ChestDeleteInterval is in seconds.\n";
 	
-	public boolean drops;
-	public boolean mineabledrops;
-	public boolean deathMessage;
-	public String deathMessageString;
-	public boolean SignOnChest;
-	public boolean LWC_Enabled;
-	public boolean LWC_PrivateDefault;
-	public boolean Sign_BeaconEnabled;
-	public int Sign_BeaconHeight;
-	public boolean LiquidReplace;
-	public int ChestDeleteInterval;
-	public boolean ChestDeleteIntervalEnabled;
+	public boolean drops = true;
+	public boolean mineabledrops = false;
+	public boolean deathMessage = true;
+	public String deathMessageString = "died and left a chest where he died.";
+	public boolean SignOnChest = true;
+	public boolean LWC_Enabled = true;
+	public boolean LWC_PrivateDefault =true;
+	public boolean Sign_BeaconEnabled = true;
+	public int Sign_BeaconHeight = 10;
+	public boolean LiquidReplace = true;
+	public int ChestDeleteInterval = 80;
+	public boolean ChestDeleteIntervalEnabled = true;
+
+	private static PermissionHandler Permissions;
 	
 	@Override
 	public void onDisable()
 	{
-		logger.log(Level.INFO, "PCD unloaded.");
+		logger.log(Level.INFO, "[DeadMansChest] unloaded.");
 		
 	}
 
 	@Override
 	public void onEnable()
 	{
+		setupPermissions();
 		Plugin lwcPlugin = getServer().getPluginManager().getPlugin("LWC");
 		if(lwcPlugin != null) {
-			System.out.println("[PlayerChestDeath] LWC plugin found!");
+			System.out.println("[DeadMansChest] LWC plugin found!");
 		    lwc = ((LWCPlugin) lwcPlugin).getLWC();
 		}
-		logger.log(Level.INFO, "PCD loaded.");
+		logger.log(Level.INFO, "[DeadMansChest] loaded.");
 		new File(mainDir).mkdir();
 		
 		if(!configFile.exists())
@@ -75,7 +81,7 @@ public class ChestDeath extends JavaPlugin
 			{
 				configFile.createNewFile();
 				FileOutputStream out = new FileOutputStream(configFile);
-				prop.put("DropsEnabled", "false");
+				prop.put("DropsEnabled", "true");
 				prop.put("DeathMessage", "true");
 				prop.put("DeathMessageString", "died and left a chest where he died.");
 				prop.put("SignOnChest", "true");
@@ -133,5 +139,24 @@ public class ChestDeath extends JavaPlugin
 		pm.registerEvent(Event.Type.BLOCK_PISTON_EXTEND, new CdBlockListener(this), Event.Priority.Monitor, this);
 		pm.registerEvent(Event.Type.BLOCK_PISTON_RETRACT, new CdBlockListener(this), Event.Priority.Monitor, this);
 	}
+    
+    private void setupPermissions() {
+        Plugin permissions = this.getServer().getPluginManager().getPlugin("Permissions");
+
+        if (Permissions == null) {
+            if (permissions != null) {
+                Permissions = ((Permissions)permissions).getHandler();
+            } else {
+            }
+        }
+    }
+    
+    public boolean hasPermissions(Player player, String node) {
+        if (Permissions != null) {
+            return Permissions.has(player, node);
+        } else {
+            return player.hasPermission(node);
+        }
+    }
 
 }
--- src/com/wesnc/playerchestdeath/EntLis.java
+++ src/com/wesnc/playerchestdeath/EntLis.java
@@ -35,161 +35,164 @@ public class EntLis extends EntityListener
 		if(entity instanceof Player)
 		{
 			Player player = (Player)entity;
-			Location lastLoc = player.getLocation();
-			Block block = lastLoc.getBlock();
+			if(plugin.hasPermissions(player, "deadmanschest.chest")) {
 
-			LinkedList<Block> changedblocks = new LinkedList<Block>();
-			Block tsignblock = null;
-			//chest related
-			block.setType(Material.CHEST);
-			changedblocks.add(block);
-			plugin.nodropblocks.add(block);
-			BlockState state = block.getState();
-			Chest chest = (Chest)state;
-			Block protectionblock = null;
-			
-			Location chestLocation = chest.getBlock().getLocation();
-			
-			int j = 0;
-			List<ItemStack> items = event.getDrops();
-			for(int i = 0; i < items.size() && j < 27; i++)
-			{
-				ItemStack item = items.get(i);
-				if(item != null && item.getType() != Material.AIR) {
-					System.out.println("Adding item " + item.getType().toString() + " to chest.");
-					chest.getInventory().addItem(item);
-					items.remove(i);
-					//A little hack to make sure the pointer is pointing to the right place...
-					i--;
-					j++;
-				}
-			}
-			
-			if(!this.plugin.drops)
-			{
-				event.getDrops().clear();
-			}
-			
-			if(this.plugin.deathMessage)
-			{
-				this.plugin.getServer().broadcastMessage(ChatColor.RED + player.getDisplayName() + ChatColor.WHITE + " " + this.plugin.deathMessageString);
-			}
-			
-			if(this.plugin.LWC_Enabled && plugin.lwc != null)
-			{
-				int blockId = chest.getTypeId();
-				int type = 0;
-				String world = chest.getWorld().getName();
-				String owner = player.getName();
-				String password = "";
-				int x = chest.getX();
-				int y = chest.getY();
-				int z = chest.getZ();
+				Location lastLoc = player.getLocation();
+				Block block = lastLoc.getBlock();
+
+				LinkedList<Block> changedblocks = new LinkedList<Block>();
+				Block tsignblock = null;
+				//chest related
+				block.setType(Material.CHEST);
+				changedblocks.add(block);
+				plugin.nodropblocks.add(block);
+				BlockState state = block.getState();
+				Chest chest = (Chest)state;
+				Block protectionblock = null;
 				
-				if(this.plugin.LWC_PrivateDefault)
+				Location chestLocation = chest.getBlock().getLocation();
+				
+				int j = 0;
+				List<ItemStack> items = event.getDrops();
+				for(int i = 0; i < items.size() && j < 27; i++)
 				{
-					type = com.griefcraft.model.ProtectionTypes.PRIVATE;
-				}
-				else
-				{
-					type = com.griefcraft.model.ProtectionTypes.PUBLIC;
-				}
-				plugin.lwc.getPhysicalDatabase().registerProtection(blockId, type, world, owner, password, x, y, z);
-				protectionblock = block;
-			}
-			
-			if(this.plugin.SignOnChest)
-			{
-				boolean foundair = false;
-				BlockFace[] directions = {BlockFace.EAST,BlockFace.WEST,BlockFace.NORTH,BlockFace.SOUTH};
-				byte[] signbyte = {0x2,0x3,0x4,0x5};
-				int signdirection = 1;
-				for(int i = 0; i < directions.length && !foundair; i++) {
-					if(plugin.LiquidReplace) {
-						//If we can replace water, let's do it with the sign too!
-						Block tempblock = block.getRelative(directions[i]);
-						if(tempblock.getType() == Material.AIR|| tempblock.getType() == Material.WATER 
-								|| tempblock.getType() == Material.STATIONARY_WATER
-								|| tempblock.getType() == Material.LAVA 
-								|| tempblock.getType() == Material.STATIONARY_LAVA) {
-							signdirection = i;
-							foundair = true;
-						}
-					}else {
-						if(block.getRelative(directions[i]).getType() == Material.AIR) {
-							signdirection = i;
-							foundair = true;
-						}
+					ItemStack item = items.get(i);
+					if(item != null && item.getType() != Material.AIR) {
+						System.out.println("Adding item " + item.getType().toString() + " to chest.");
+						chest.getInventory().addItem(item);
+						items.remove(i);
+						//A little hack to make sure the pointer is pointing to the right place...
+						i--;
+						j++;
 					}
 				}
 				
-				if(foundair) {
-					//-----------------------------------------------------------
-					Block signBlock = block.getRelative(directions[signdirection]);
-					signBlock.setTypeIdAndData(68, signbyte[signdirection], false);
-					BlockState signState = signBlock.getState();
-					Sign sign = (Sign)signState;
-					//-----------------------------------------------------------
-					
-					sign.setLine(0, player.getDisplayName()+"'s");
-					sign.setLine(1, "Deathpile");
-					sign.update();
-					changedblocks.add(signBlock);
-					plugin.nodropblocks.add(signBlock);
-					plugin.signblocks.put(block, signBlock);
-				}else {
-					// If we didn't find a free spot, let's put the sign above the chest...
-					// Will probably look very ugly though and pop off anyways...
-					//-----------------------------------------------------------
-					Block signBlock = block.getRelative(BlockFace.UP);
-					signBlock.setTypeIdAndData(63, signbyte[signdirection], false);
-					BlockState signState = signBlock.getState();
-					Sign sign = (Sign)signState;
-					//-----------------------------------------------------------
-					
-					sign.setLine(0, player.getDisplayName()+"'s");
-					sign.setLine(1, "Deathpile");
-					sign.update();
-					changedblocks.add(signBlock);
-					plugin.nodropblocks.add(signBlock);
-					plugin.signblocks.put(block, signBlock);
+				if(!this.plugin.drops && !plugin.hasPermissions(player, "deadmanschest.drops"))
+				{
+					event.getDrops().clear();
 				}
-			}
-			
-			if(this.plugin.Sign_BeaconEnabled)
-			{
-				int height = this.plugin.Sign_BeaconHeight;
-				Location chestLocation1 = block.getLocation();
 				
-				Location firstlocation = chestLocation1.add(0.0, 2.0, 0.0);
-				Block nextblock = firstlocation.getBlock();
+				if(this.plugin.deathMessage && plugin.hasPermissions(player, "deadmanschest.message"))
+				{
+					this.plugin.getServer().broadcastMessage(ChatColor.RED + player.getDisplayName() + ChatColor.WHITE + " " + this.plugin.deathMessageString);
+				}
 				
-				for(int i = 0; i < height; i++) {
-					if(plugin.LiquidReplace) {
-						if(nextblock.getType() == Material.AIR || nextblock.getType() == Material.WATER 
-								|| nextblock.getType() == Material.STATIONARY_WATER
-								|| nextblock.getType() == Material.LAVA 
-								|| nextblock.getType() == Material.STATIONARY_LAVA ) {
-							nextblock.setType(Material.GLOWSTONE);
-							plugin.nodropblocks.add(nextblock);
-							changedblocks.add(nextblock);
-						}
-					}else {
-						if(nextblock.getType() == Material.AIR) {
-							nextblock.setType(Material.GLOWSTONE);
-							plugin.nodropblocks.add(nextblock);
-							changedblocks.add(nextblock);
+				if(this.plugin.LWC_Enabled && plugin.lwc != null && plugin.hasPermissions(player, "deadmanschest.lock"))
+				{
+					int blockId = chest.getTypeId();
+					int type = 0;
+					String world = chest.getWorld().getName();
+					String owner = player.getName();
+					String password = "";
+					int x = chest.getX();
+					int y = chest.getY();
+					int z = chest.getZ();
+					
+					if(this.plugin.LWC_PrivateDefault)
+					{
+						type = com.griefcraft.model.ProtectionTypes.PRIVATE;
+					}
+					else
+					{
+						type = com.griefcraft.model.ProtectionTypes.PUBLIC;
+					}
+					plugin.lwc.getPhysicalDatabase().registerProtection(blockId, type, world, owner, password, x, y, z);
+					protectionblock = block;
+				}
+				
+				if(this.plugin.SignOnChest)
+				{
+					boolean foundair = false;
+					BlockFace[] directions = {BlockFace.EAST,BlockFace.WEST,BlockFace.NORTH,BlockFace.SOUTH};
+					byte[] signbyte = {0x2,0x3,0x4,0x5};
+					int signdirection = 1;
+					for(int i = 0; i < directions.length && !foundair; i++) {
+						if(plugin.LiquidReplace) {
+							//If we can replace water, let's do it with the sign too!
+							Block tempblock = block.getRelative(directions[i]);
+							if(tempblock.getType() == Material.AIR|| tempblock.getType() == Material.WATER 
+									|| tempblock.getType() == Material.STATIONARY_WATER
+									|| tempblock.getType() == Material.LAVA 
+									|| tempblock.getType() == Material.STATIONARY_LAVA) {
+								signdirection = i;
+								foundair = true;
+							}
+						}else {
+							if(block.getRelative(directions[i]).getType() == Material.AIR) {
+								signdirection = i;
+								foundair = true;
+							}
 						}
 					}
-					nextblock = nextblock.getRelative(BlockFace.UP);
+					
+					if(foundair) {
+						//-----------------------------------------------------------
+						Block signBlock = block.getRelative(directions[signdirection]);
+						signBlock.setTypeIdAndData(68, signbyte[signdirection], false);
+						BlockState signState = signBlock.getState();
+						Sign sign = (Sign)signState;
+						//-----------------------------------------------------------
+						
+						sign.setLine(0, player.getDisplayName()+"'s");
+						sign.setLine(1, "Deathpile");
+						sign.update();
+						changedblocks.add(signBlock);
+						plugin.nodropblocks.add(signBlock);
+						plugin.signblocks.put(block, signBlock);
+					}else {
+						// If we didn't find a free spot, let's put the sign above the chest...
+						// Will probably look very ugly though and pop off anyways...
+						//-----------------------------------------------------------
+						Block signBlock = block.getRelative(BlockFace.UP);
+						signBlock.setTypeIdAndData(63, signbyte[signdirection], false);
+						BlockState signState = signBlock.getState();
+						Sign sign = (Sign)signState;
+						//-----------------------------------------------------------
+						
+						sign.setLine(0, player.getDisplayName()+"'s");
+						sign.setLine(1, "Deathpile");
+						sign.update();
+						changedblocks.add(signBlock);
+						plugin.nodropblocks.add(signBlock);
+						plugin.signblocks.put(block, signBlock);
+					}
 				}
-			}
-			
-			if(this.plugin.ChestDeleteIntervalEnabled)
-			{
-				int delay = this.plugin.ChestDeleteInterval*20;
-				this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new RemoveChest(plugin, changedblocks, protectionblock), delay);
 				
+				if(this.plugin.Sign_BeaconEnabled && plugin.hasPermissions(player, "deadmanschest.beacon"))
+				{
+					int height = this.plugin.Sign_BeaconHeight;
+					Location chestLocation1 = block.getLocation();
+					
+					Location firstlocation = chestLocation1.add(0.0, 2.0, 0.0);
+					Block nextblock = firstlocation.getBlock();
+					
+					for(int i = 0; i < height; i++) {
+						if(plugin.LiquidReplace) {
+							if(nextblock.getType() == Material.AIR || nextblock.getType() == Material.WATER 
+									|| nextblock.getType() == Material.STATIONARY_WATER
+									|| nextblock.getType() == Material.LAVA 
+									|| nextblock.getType() == Material.STATIONARY_LAVA ) {
+								nextblock.setType(Material.GLOWSTONE);
+								plugin.nodropblocks.add(nextblock);
+								changedblocks.add(nextblock);
+							}
+						}else {
+							if(nextblock.getType() == Material.AIR) {
+								nextblock.setType(Material.GLOWSTONE);
+								plugin.nodropblocks.add(nextblock);
+								changedblocks.add(nextblock);
+							}
+						}
+						nextblock = nextblock.getRelative(BlockFace.UP);
+					}
+				}
+				
+				if(this.plugin.ChestDeleteIntervalEnabled && !plugin.hasPermissions(player, "deadmanschest.nodelete"))
+				{
+					int delay = this.plugin.ChestDeleteInterval*20;
+					this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new RemoveChest(plugin, changedblocks, protectionblock), delay);
+					
+				}
 			}
 	
 		}
